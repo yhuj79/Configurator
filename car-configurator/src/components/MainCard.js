61b267b4e0104e8bf6c905086d080844fb849410
yhuj79 <yu1085@naver.com>
2022-06-22 03:50:18 +0900

    Main ~Configure

--- car-configurator/src/components/MainCard.js
+++ car-configurator/src/components/MainCard.js
@@ -1,14 +1,79 @@
-// import styled from "styled-components";
+import styled from "styled-components";
 
-// function MainCard() {
-//   return (
-//     <MainCardDiv>
-//       <img className="main-img1" alt="" src={MainImageOne} />
-//       <div className="main-text">
-//         <p className="main-text-title">C-Class</p>
-//       </div>
-//     </MainCardDiv>
-//   );
-// }
-// const MainCardDiv = styled.div``;
-// export default MainCard;
+function MainCard({ image, name, delayTime }) {
+  return (
+    <MainCardDiv style={{ animationDelay: `${delayTime}s` }}>
+      <img className="card-img" alt="" src={image} />
+      <p className="card-button">Configure</p>
+      <div className="card-text">
+        <p className="card-text-title">{name}</p>
+      </div>
+    </MainCardDiv>
+  );
+}
+const MainCardDiv = styled.div`
+  width: 50%;
+  margin: 0 8px 16px;
+  opacity: 0;
+  animation: AppearSmooth 1.2s;
+  animation-fill-mode: forwards;
+
+  .card-img {
+    width: 100%;
+    opacity: 0.7;
+    transition: all 0.5s;
+  }
+  .card-button {
+    color: black;
+    position: absolute;
+    top: 5%;
+    right: -20%;
+    opacity: 0;
+    animation: AppearSmooth 1.2s;
+    animation-delay: 3.5s;
+    animation-fill-mode: forwards;
+    transition: all 0.3s;
+  }
+  .card-text {
+    position: absolute;
+    bottom: 5%;
+    left: 3%;
+  }
+  .card-text-title {
+    font-family: "Playfair Display", serif;
+    font-size: 35px;
+    font-weight: bold;
+    text-shadow: 1px 1px 1px black;
+    opacity: 0;
+    animation: AppearSmooth 1.2s;
+    animation-delay: 3.5s;
+    animation-fill-mode: forwards;
+    transition: all 0.3s;
+  }
+  &:hover > .card-img {
+    opacity: 1;
+    transition: all 0.5s;
+  }
+  &:hover > .card-button {
+    color: white;
+    opacity: 1;
+    right: 3%;
+    transition: all 0.3s;
+  }
+  &:hover .card-text-title {
+    font-size: 50px;
+    transition: all 0.3s;
+  }
+
+  @keyframes AppearSmooth {
+    from {
+      opacity: 0;
+      transform: translateY(-5%);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+`;
+export default MainCard;
--- car-configurator/src/components/MainContent.js
+++ car-configurator/src/components/MainContent.js
@@ -1,40 +1,33 @@
 import styled from "styled-components";
 
-import MainImageOne from "../img/Main_Cclass.png";
-import MainImageTwo from "../img/Main_Amg.png";
-import MainImageThree from "../img/Main_Cla.jpg";
-import MainImageFour from "../img/Main_Sclass.jpg";
+import MainCard from "./MainCard";
 
 function MainContent() {
   return (
     <MainContentDiv>
       <div className="main-container">
-        <div className="main-img-section1">
-          <img className="main-img1" alt="" src={MainImageOne} />
-          <div className="main-text">
-            <p className="main-text-title1">C-Class</p>
-          </div>
-        </div>
-        <div className="main-img-section2">
-          <img className="main-img2" alt="" src={MainImageTwo} />
-          <div className="main-text">
-            <p className="main-text-title2">AMG</p>
-          </div>
-        </div>
+        <MainCard
+          image={require("../img/Main_Cclass.png")}
+          name="C-Class"
+          delayTime={1.5}
+        />
+        <MainCard
+          image={require("../img/Main_Amg.png")}
+          name="AMG"
+          delayTime={2.0}
+        />
       </div>
       <div className="main-container">
-        <div className="main-img-section3">
-          <img className="main-img3" alt="" src={MainImageThree} />
-          <div className="main-text">
-            <p className="main-text-title3">CLA-Class</p>
-          </div>
-        </div>
-        <div className="main-img-section4">
-          <img className="main-img4" alt="" src={MainImageFour} />
-          <div className="main-text">
-            <p className="main-text-title4">S-Class</p>
-          </div>
-        </div>
+        <MainCard
+          image={require("../img/Main_Cla.jpg")}
+          name="CLA-Class"
+          delayTime={2.5}
+        />
+        <MainCard
+          image={require("../img/Main_Sclass.jpg")}
+          name="S-Class"
+          delayTime={3.0}
+        />
       </div>
     </MainContentDiv>
   );
@@ -46,79 +39,6 @@ const MainContentDiv = styled.div`
   .main-container {
     display: flex;
     position: relative;
-  }
-  .main-img-section1,
-  .main-img-section2,
-  .main-img-section3,
-  .main-img-section4 {
-    width: 50%;
-    margin: 0 8px 16px;
-    opacity: 0;
-    animation: AppearSmooth 1.2s;
-    animation-fill-mode: forwards;
-  }
-  .main-img1,
-  .main-img2,
-  .main-img3,
-  .main-img4 {
-    width: 100%;
-    transition: all 0.5s;
-  }
-  .main-img-section1:hover > .main-img1,
-  .main-img-section2:hover > .main-img2,
-  .main-img-section3:hover > .main-img3,
-  .main-img-section4:hover > .main-img4 {
-    opacity: 0.7;
-    transition: all 0.5s;
-  }
-  .main-text {
-    position: absolute;
-    bottom: 5%; left: 3%;
-  }
-  .main-text-title1,
-  .main-text-title2,
-  .main-text-title3,
-  .main-text-title4 {
-    font-family: 'Playfair Display', serif;
-    font-size: 35px;
-    font-weight: bold;
-    text-shadow: 1px 1px 1px black;
-    opacity: 0;
-    animation: AppearSmooth 1.2s;
-    animation-delay: 3.5s;
-    animation-fill-mode: forwards;
-    transition: all 0.3s;
-  }
-  .main-img-section1:hover .main-text-title1,
-  .main-img-section2:hover .main-text-title2,
-  .main-img-section3:hover .main-text-title3,
-  .main-img-section4:hover .main-text-title4 {
-    font-size: 50px;
-    transition: all 0.3s;
-  }
-
-  @keyframes AppearSmooth {
-    from {
-      opacity: 0;
-      transform: translateY(-5%);
-    }
-    to {
-      opacity: 1;
-      transform: translateY(0);
-    }
-  }
-
-  .main-img-section1 {
-    animation-delay: 1.5s;
-  }
-  .main-img-section2 {
-    animation-delay: 2s;
-  }
-  .main-img-section3 {
-    animation-delay: 2.5s;
-  }
-  .main-img-section4 {
-    animation-delay: 3s;
   }
 `;
 export default MainContent;
