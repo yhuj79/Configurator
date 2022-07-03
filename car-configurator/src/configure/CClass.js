c12479cf2195db4181791db92027ffbcce272e0b
yhuj79 <yu1085@naver.com>
2022-07-04 07:13:17 +0900

    Configure OnDragMove Test 1

--- car-configurator/src/configure/CClass.js
+++ car-configurator/src/configure/CClass.js
@@ -1,29 +1,69 @@
 import styled from "styled-components";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 function CClass() {
+  const scrollRef = useRef(null);
+  const [isDrag, setIsDrag] = useState(false);
+  const [startX, setStartX] = useState();
   const [degree, setDegree] = useState(1);
-  const degreeLeft = () => {
-    degree === 36 ? setDegree(1) : setDegree(degree + 1);
+
+  const onDragStart = (e) => {
+    e.preventDefault();
+    setIsDrag(true);
+    setStartX(e.pageX + scrollRef.current.scrollLeft);
   };
-  const degreeRight = () => {
-    degree === 1 ? setDegree(36) : setDegree(degree - 1);
+
+  const onDragEnd = () => {
+    setIsDrag(false);
   };
+
+  const throttle = (func, ms) => {
+    let throttled = false;
+    return (...args) => {
+      if (!throttled) {
+        throttled = true;
+        setTimeout(() => {
+          func(...args);
+          throttled = false;
+        }, ms);
+      }
+    };
+  };
+
+  const onDragMove = (e) => {
+    if (isDrag) {
+      const { scrollWidth, clientWidth, scrollLeft } = scrollRef.current;
+
+      if (scrollLeft === 0) {
+        setStartX(e.pageX);
+      } else if (scrollWidth < clientWidth + scrollLeft) {
+        setStartX(e.pageX + scrollLeft);
+      }
+
+      if (startX - e.pageX > 0) {
+        degree === 36 ? setDegree(1) : setDegree(degree + 1);
+      } else if (startX - e.pageX < 0) {
+        degree === 1 ? setDegree(36) : setDegree(degree - 1);
+      }
+    }
+  };
+
+  const delay = 60;
+  const onThrottleDragMove = throttle(onDragMove, delay);
 
   return (
     <ConfigureDiv>
       <h1>C Class [ TEST ]</h1>
       <img
+        onMouseDown={onDragStart}
+        onMouseMove={onThrottleDragMove}
+        onMouseUp={onDragEnd}
+        onMouseLeave={onDragEnd}
+        ref={scrollRef}
         className="configure-img"
         alt=""
         src={require(`./img/cclass/cclass${degree}.webp`)}
       />
-      <button className="configure-degree-button" onClick={degreeLeft}>
-        &lt;&lt;
-      </button>
-      <button className="configure-degree-button" onClick={degreeRight}>
-        &gt;&gt;
-      </button>
     </ConfigureDiv>
   );
 }
