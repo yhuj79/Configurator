0c1ae1cabf9bdc210f643d7262266e3fc6bd927a
yhuj79 <yu1085@naver.com>
2022-07-06 08:52:25 +0900

    Configure 분리

--- car-configurator/src/configure/Amg.js
+++ car-configurator/src/configure/Amg.js
@@ -1,7 +1,67 @@
+import styled from "styled-components";
+import { useState } from "react";
 import Configure from "./Configure";
 
 function Amg() {
-  return <Configure name="amg" />;
-}
+  const [pack, setPack] = useState("nsp");
+  const [color, setColor] = useState("polawhite");
+  const [wheel, setWheel] = useState("20alloy");
 
+  return (
+    <ConfigureAmgDiv>
+      <div className="configure-option-section">
+        <h1>옵션</h1>
+        <div>
+          <h2>V8 스타일 팩</h2>
+          <div>
+            <p className="configure-option-button" onClick={() => { setPack("sp"); }} >YES</p>
+            <p className="configure-option-button"
+              onClick={(color === "orange") ? {} : (() => { setPack("nsp"); })}
+              style={(color === "orange") ? {opacity: "0.7"}: {}}
+            >NO
+            </p>
+          </div>
+        </div>
+        <div>
+          <h2>Color</h2>
+          <div>
+            <p className="configure-option-button" onClick={() => { setColor("polawhite"); }}>Pola White</p>
+            <p className="configure-option-button" onClick={() => { setColor("black"); }}>Obsidian Black</p>
+            <p className="configure-option-button"
+              onClick={(pack === "sp") ? (() => { setColor("orange"); }) : {}}
+              style={(pack === "sp") ? {}: {opacity: "0.7"}}
+            >
+              Orange<span style={(pack === "sp") ? {display: "none"}: {}}> (V8 스타일 팩 필요)</span>
+            </p>
+          </div>
+        </div>
+        <div>
+          <h2>Wheel</h2>
+          <div>
+            <p className="configure-option-button" onClick={() => { setWheel("20alloy"); }} style={(wheel === "20alloy") ? {backgroundColor: "green"} : {}}>
+              20인치 AMG 5트윈 스포크 경량 알로이 휠
+            </p>
+            <p className="configure-option-button" onClick={() => { setWheel("20multialloy"); }} style={(wheel === "20multialloy") ? {backgroundColor: "green"} : {}}>
+              20인치 AMG 멀티 스포크 경량 알로이 휠
+            </p>
+          </div>
+        </div>
+      </div>
+      <Configure name="amg" pack={pack} color={color} wheel={wheel} />
+    </ConfigureAmgDiv>
+  );
+}
+const ConfigureAmgDiv = styled.div`
+  width: 80%;
+  margin: auto;
+  display: flex;
+  font-size: 20px;
+
+  .configure-option-button {
+    background-color: gray;
+    border: 2px solid white;
+    border-radius: 7px;
+    padding: 20px;
+  }
+`;
 export default Amg;
--- car-configurator/src/configure/Configure.js
+++ car-configurator/src/configure/Configure.js
@@ -1,16 +1,12 @@
 import styled from "styled-components";
 import { useState, useRef } from "react";
 
-function Configure({ name }) {
+function Configure({ name, pack, color, wheel }) {
   const imgRef = useRef(null);
   const [isDrag, setIsDrag] = useState(false);
   const [startX, setStartX] = useState();
   const [degree, setDegree] = useState(1);
   const [throttle, setThrottle] = useState(0);
-
-  const [pack, setPack] = useState("nsp");
-  const [color, setColor] = useState("polawhite");
-  const [wheel, setWheel] = useState("20alloy");
 
   function onDragStart(e) {
     e.preventDefault();
@@ -63,116 +59,29 @@ function Configure({ name }) {
 
   return (
     <ConfigureDiv>
-      <div className="configure-option-section">
-        <h1>옵션</h1>
-        <div>
-          <h2>V8 스타일 팩</h2>
-          <div>
-            <p
-              className="configure-option-button"
-              onClick={() => {
-                setPack("sp");
-              }}
-            >
-              YES
-            </p>
-            <p
-              className="configure-option-button"
-              onClick={() => {
-                setPack("nsp");
-              }}
-            >
-              NO
-            </p>
-          </div>
-        </div>
-        <div>
-          <h2>Color</h2>
-          <div>
-            <p
-              className="configure-option-button"
-              onClick={() => {
-                setColor("polawhite");
-              }}
-            >
-              Pola White
-            </p>
-            <p
-              className="configure-option-button"
-              onClick={() => {
-                setColor("black");
-              }}
-            >
-              Obsidian Black
-            </p>
-            <p
-              className="configure-option-button"
-              onClick={() => {
-                setColor("orange");
-              }}
-            >
-              Orange
-            </p>
-          </div>
-        </div>
-        <div>
-          <h2>Wheel</h2>
-          <div>
-            <p
-              className="configure-option-button"
-              onClick={() => {
-                setWheel("20alloy");
-              }}
-            >
-              20인치 AMG 5트윈 스포크 경량 알로이 휠
-            </p>
-            <p
-              className="configure-option-button"
-              onClick={() => {
-                setWheel("20multialloy");
-              }}
-            >
-              20인치 AMG 멀티 스포크 경량 알로이 휠
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="configure-img-section">
-        <h1>TEST</h1>
-        <h1>amg-sp-polawhite-20multialloy-</h1>
-        {/* <h1>{name} [ TEST ]</h1> */}
-        <img
-          onMouseDown={onDragStart}
-          onMouseMove={onDragMove}
-          onMouseUp={onDragEnd}
-          onMouseLeave={onDragEnd}
-          ref={imgRef}
-          className="configure-img"
-          alt=""
-          src={require(`./file/${name}/${name}-${pack}-${color}-${wheel}-${degree}.webp`)}
-        />
-      </div>
+      <h1>TEST</h1>
+      <h1>amg-sp-polawhite-20multialloy-</h1>
+      <img
+        className="configure-img"
+        alt=""
+        ref={imgRef}
+        onMouseDown={onDragStart}
+        onMouseMove={onDragMove}
+        onMouseUp={onDragEnd}
+        onMouseLeave={onDragEnd}
+        src={require(`./file/${name}/${name}-${pack}-${color}-${wheel}-${degree}.webp`)}
+      />
     </ConfigureDiv>
   );
 }
 const ConfigureDiv = styled.div`
-  width: 80%;
-  margin: auto;
-  display: flex;
-  font-size: 20px;
+  width: 85%;
 
-  .configure-option-section {
-    width: 15%;
-    margin-right: 35px;
-  }
   .configure-option-button {
     background-color: gray;
     border: 2px solid white;
     border-radius: 7px;
     padding: 20px;
-  }
-  .configure-img-section {
-    width: 85%;
   }
   .configure-img {
     width: 100%;
