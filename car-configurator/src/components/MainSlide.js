5d20261b40eb18c60b390baaa1cfd068c5da5123
yhuj79 <yu1085@naver.com>
2022-07-16 11:35:19 +0900

    MainSlide2

--- car-configurator/src/components/MainSlide.js
+++ car-configurator/src/components/MainSlide.js
@@ -1,25 +1,44 @@
+import { useState } from "react";
 import styled from "styled-components";
-import SlideAmg from "../img/Slide_Amg.webp";
-// import SlideSclass from "../img/Slide_Sclass.webp";
-// import SlideCclass from "../img/Slide_Cclass.webp";
-// import SlideCla from "../img/Slide_Cla.webp";
 
 function MainSlide() {
+  const [imgNum, setImgNum] = useState(1);
+  // setInterval(function () {
+  //   imgNum === 4 ? setImgNum(1) : setImgNum(imgNum + 1);
+  // }, 2000);
+
   return (
     <MainSlideDiv>
-      <Img alt="" src={SlideAmg} />
-      {/* <Img alt="" src={SlideSclass} />
-      <Img alt="" src={SlideCclass} />
-      <Img alt="" src={SlideCla} /> */}
+      <img
+        className="slide-img"
+        alt=""
+        src={require(`../img/Slide-${imgNum}.webp`)}
+      />
+      <p className="slide-title">
+        The all-new
+        <br />
+        Mercedes-AMG GT 4-Door Coupé
+      </p>
     </MainSlideDiv>
   );
 }
 const MainSlideDiv = styled.div`
+  position: relative;
+  width: 80%;
   margin: auto;
-  text-align: center;
   margin-bottom: 30px;
-`;
-const Img = styled.img`
-  width: 100%;
+
+  .slide-img {
+    width: 100%;
+  }
+  .slide-title {
+    position: absolute;
+    font-family: "Playfair Display", serif;
+    font-size: 40px;
+    font-weight: bold;
+    text-shadow: 1px 1px 1px black;
+    top: 5%;
+    left: 3%;
+  }
 `;
 export default MainSlide;
